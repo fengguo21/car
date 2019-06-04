020a035ba92097d7f37e095d3ab6e73e8253ce8f
renyakun <renyakun@optaim.com>
2019-06-04 14:12:35 +0800

    init

---
+++ pages/detail/detail.js
@@ -0,0 +1,81 @@
+// pages/detail/detail.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    isChecked:false,
+  },
+  check: function (e) {
+    this.setData({
+      isChecked: true
+    });
+  },
+  confirm(){
+    wx.navigateTo({
+      url: "/pages/binded/binded"
+    })
+  },
+  recheck(){
+    this.setData({
+      isChecked: false
+    });
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
